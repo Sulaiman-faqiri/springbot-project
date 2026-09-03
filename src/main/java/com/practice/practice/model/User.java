5b9fb4d62ca886566833dd3ec7ea35118e681214
Sulaiman-faqiri <sulaimanfaqiri2001@gmail.com>
2026-09-03 11:48:40 +0430

    rafactored user model  with lambok

--- src/main/java/com/practice/practice/model/User.java
+++ src/main/java/com/practice/practice/model/User.java
@@ -1,38 +1,38 @@
-
 package com.practice.practice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "age")
     private int age;
+    @Column(name = "email", unique = true)
     private String email;
 
-    public User() {}
-
-    public User(String name,int age, String email){
-        this.name=name;
-        this.age=age;
-        this.email=email;
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
     }
-    public Long getId(){return id;}
-
-    public String getName(){return name;}
-    public void setName(String name){this.name=name;}
-
-    public int getAge(){return age;}
-    public void setAge(int age){this.age=age;}
-
-    public String getEmail(){return email;}
-    public void setEmail(String email){this.email=email;}
 }
